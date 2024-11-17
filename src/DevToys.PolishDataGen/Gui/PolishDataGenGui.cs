92281c0b1c09e7e6e49aa6949ca0c024874e907e
Michał Jackowski <mjackowski191@gmail.com>
2024-11-17 22:10:00 +0100

    Integration with DevToys.Api

---
+++ src/DevToys.PolishDataGen/Gui/PolishDataGenGui.cs
@@ -0,0 +1,27 @@
+﻿using DevToys.Api;
+using System.ComponentModel.Composition;
+using static DevToys.Api.GUI;
+
+namespace DevToys.PolishDataGen.Gui;
+
+[Export(typeof(IGuiTool))]
+[Name("PolishDataGen")]
+[ToolDisplayInformation(
+    IconFontName = "FluentSystemIcons",
+    IconGlyph = '\uE670',
+    GroupName = PredefinedCommonToolGroupNames.Generators,
+    ResourceManagerAssemblyIdentifier = nameof(PolishDataGenResourceAssemblyIdentifier),
+    ResourceManagerBaseName = "DevToys.PolishDataGen.Strings.PolishDataGen",
+    ShortDisplayTitleResourceName = nameof(Strings.PolishDataGen.ShortDisplayTitle),
+    LongDisplayTitleResourceName = nameof(Strings.PolishDataGen.LongDisplayTitle),
+    DescriptionResourceName = nameof(Strings.PolishDataGen.Description),
+    AccessibleNameResourceName = nameof(Strings.PolishDataGen.AccessibleName))]
+internal sealed class PolishDataGenGui : IGuiTool
+{
+    public UIToolView View => new(Label().Style(UILabelStyle.BodyStrong).Text(Strings.PolishDataGen.PolishDataGenLabel));
+
+    public void OnDataReceived(string dataTypeName, object? parsedData)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/DevToys.PolishDataGen/PolishDataGenResourceAssemblyIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using DevToys.Api;
+using System.ComponentModel.Composition;
+
+namespace DevToys.PolishDataGen;
+
+[Export(typeof(IResourceAssemblyIdentifier))]
+[Name(nameof(PolishDataGenResourceAssemblyIdentifier))]
+internal sealed class PolishDataGenResourceAssemblyIdentifier : IResourceAssemblyIdentifier
+{
+    public ValueTask<FontDefinition[]> GetFontDefinitionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
