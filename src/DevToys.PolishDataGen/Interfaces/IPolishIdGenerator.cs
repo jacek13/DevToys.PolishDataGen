f5ca994e058c76aad0aa3d15a9a584cbd2df33ea
Michał Jackowski <mjackowski191@gmail.com>
2024-11-17 22:51:08 +0100

    Added IPolishIdGenerator

---
+++ src/DevToys.PolishDataGen/Interfaces/IPolishIdGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace DevToys.PolishDataGen.Interfaces;
+
+public interface IPolishIdGenerator
+{
+    /// <summary>
+    /// Property that holds the control mask used for generating the check digit.
+    /// </summary>
+    ushort[] ControlMask { get; }
+
+    /// <summary>
+    /// Creates a single identifier according to the rules of the specific generator.
+    /// </summary>
+    /// <returns>A single valid string representing the identifier.</returns>
+    string Create();
+
+    /// <summary>
+    /// Creates a specified number of identifiers.
+    /// </summary>
+    /// <param name="count">The number of identifiers to generate.</param>
+    /// <returns>A collection of generated identifiers.</returns>
+    IReadOnlyCollection<string> CreateMany(int count);
+}
