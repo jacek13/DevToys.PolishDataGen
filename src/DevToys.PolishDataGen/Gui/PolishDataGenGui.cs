20ba1bf41874f6df5e89cf155c1d53aa0c8b0e63
Michał Jackowski <mjackowski191@gmail.com>
2024-12-02 19:41:00 +0100

    Added basic GUI template

--- src/DevToys.PolishDataGen/Gui/PolishDataGenGui.cs
+++ src/DevToys.PolishDataGen/Gui/PolishDataGenGui.cs
@@ -18,10 +18,39 @@ namespace DevToys.PolishDataGen.Gui;
     AccessibleNameResourceName = nameof(Strings.PolishDataGen.AccessibleName))]
 internal sealed class PolishDataGenGui : IGuiTool
 {
-    public UIToolView View => new(Label().Style(UILabelStyle.BodyStrong).Text(Strings.PolishDataGen.PolishDataGenLabel));
+    private readonly IUILabel _label = Label()
+        .Style(UILabelStyle.BodyStrong)
+        .Text(Strings.PolishDataGen.PolishDataGenLabel);
+
+    // TODO move strings to resources
+    private readonly IUIButton _buttonGenerate = Button()
+        .Text("Generate");
+
+    // TODO profile memory to find proper limits
+    private readonly IUINumberInput _numberInput = NumberInput()
+        .Text("Number of data to generate")
+        .Minimum(1)
+        .Maximum(1_000_000)
+        .Step(1.0);
+
+    private readonly IUIMultiLineTextInput _outputMultiLineText = MultiLineTextInput()
+        .Extendable()
+        .AlwaysShowLineNumber()
+        .ReadOnly();
+
+    public UIToolView View
+        => new UIToolView(
+            Stack()
+                .Vertical()
+                .WithChildren(_label, _numberInput, _buttonGenerate, _outputMultiLineText)
+        );
 
     public void OnDataReceived(string dataTypeName, object? parsedData)
     {
         throw new NotImplementedException();
     }
+
+    // TODO make OnGenerate method
+    // TODO make OnGeneratorTypeSelected method
+    // TODO make OnNumberChanged
 }
