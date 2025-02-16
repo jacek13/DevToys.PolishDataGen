e733419ac55d080f5c044d83ef11013dc797bf26
Michał Jackowski <mjackowski191@gmail.com>
2025-02-16 13:25:38 +0100

    Added NipValidatorTests; Added NipPrefixes

---
+++ src/DevToys.PolishDataGen/Providers/Validators/NipValidator.cs
@@ -0,0 +1,23 @@
+﻿using DevToys.PolishDataGen.Interfaces;
+
+namespace DevToys.PolishDataGen.Providers.Validators;
+
+public class NipValidator : IPolishIdValidator
+{
+    public ushort[] ControlMask => throw new NotImplementedException();
+
+    public int CalculateControlNumber(string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsValid(string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public (bool Result, IEnumerable<string> Messages) IsValidExplained(string value)
+    {
+        throw new NotImplementedException();
+    }
+}
